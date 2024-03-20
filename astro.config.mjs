e3ea19b4b099995a831970b06538d6c6c3d0c0a4
KeithWesley <keithfayant@gmail.com>
2024-03-20 10:11:20 -0500

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,9 +8,7 @@ import db from "@astrojs/db";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify({
-    edgeMiddleware: true,
-  }),
+  adapter: netlify(),
   integrations: [
     alpinejs({
       entrypoint: "/src/entrypoint",
--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,11 +1,29 @@
 import { ProgramsTable, db, eq } from "astro:db";
 import type { ProgramsApiResponseProps } from "../../types/databaseTypes";
+import type { APIRoute } from "astro";
 
-export async function GET({ request }: any) {
-  console.log(request);
+export const GET: APIRoute = async ({ url }): Promise<Response> => {
+  const test: string | null = url.searchParams.get("test");
+  let data;
 
-  return new Response(JSON.stringify({ message: "This was a GET!" }));
-}
+  if (test === null) {
+    return new Response(JSON.stringify({ error: "Test param is null." }), {
+      status: 400,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } else {
+    data = await getPrograms("hypertrophy");
+  }
+
+  return new Response(JSON.stringify({ message: "This was a GET, " + data }), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
 
 async function getPrograms(goal: string) {
   try {
