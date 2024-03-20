d89a590426914a3f20dd80bf5020e63e1cb37ea9
KeithWesley <keithfayant@gmail.com>
2024-03-20 09:14:55 -0500

    changes

--- src/pages/api/programs.html.ts
+++ src/pages/api/programs.html.ts
@@ -3,47 +3,61 @@ import type { ProgramsApiResponseProps } from "../../types/databaseTypes";
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async ({ request }) => {
-  const url = new URL(request.url);
-  const params = url.searchParams;
-  const goal: string | null = params.get("goal");
+  try {
+    const url = new URL(request.url);
+    const params = new URLSearchParams(url.search);
+    const goal: string | null = params.get("goal");
 
-  if (goal) {
-    const data: any = await getPrograms(goal);
+    if (goal) {
+      const data: any = await getPrograms(goal);
 
-    if (data) {
-      let htmlContent: string =
-        "<option value=''>-- Select a program --</option>";
-      htmlContent += data
-        .map(
-          (program: any) =>
-            `<option value='${program.value}'>${program.label}</option>`
-        )
-        .join("");
+      if (data) {
+        let htmlContent: string =
+          "<option value=''>-- Select a program --</option>";
+        htmlContent += data
+          .map(
+            (program: any) =>
+              `<option value='${program.value}'>${program.label}</option>`
+          )
+          .join("");
 
-      return new Response(htmlContent, {
-        status: 200,
-        headers: {
-          "Content-Type": "text/html",
-        },
-      });
+        return new Response(htmlContent, {
+          status: 200,
+          headers: {
+            "Content-Type": "text/html",
+          },
+        });
+      } else {
+        return new Response(
+          JSON.stringify({ error: "No data found for the given goal" }),
+          {
+            status: 404,
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+      }
     } else {
       return new Response(
-        JSON.stringify({ error: "No data found for the given goal" }),
+        JSON.stringify({
+          error: "Invalid query parameter",
+        }),
         {
-          status: 404,
+          status: 400,
           headers: {
             "Content-Type": "application/json",
           },
         }
       );
     }
-  } else {
+  } catch (error) {
+    console.error("Error processing request:", error);
+
     return new Response(
-      JSON.stringify({
-        error: "Invalid query parameter",
-      }),
+      JSON.stringify({ error: "Internal server error: " + error }),
       {
-        status: 400,
+        status: 500,
         headers: {
           "Content-Type": "application/json",
         },
@@ -51,70 +65,6 @@ export const GET: APIRoute = async ({ request }) => {
     );
   }
 };
-
-// export const GET: APIRoute = async ({ request }) => {
-//   try {
-//     const url = new URL(request.url);
-//     const params = new URLSearchParams(url.search);
-//     const goal: string | null = params.get("goal");
-
-//     if (goal) {
-//       const data: any = await getPrograms(goal);
-
-//       if (data) {
-//         let htmlContent: string =
-//           "<option value=''>-- Select a program --</option>";
-//         htmlContent += data
-//           .map(
-//             (program: any) =>
-//               `<option value='${program.value}'>${program.label}</option>`
-//           )
-//           .join("");
-
-//         return new Response(htmlContent, {
-//           status: 200,
-//           headers: {
-//             "Content-Type": "text/html",
-//           },
-//         });
-//       } else {
-//         return new Response(
-//           JSON.stringify({ error: "No data found for the given goal" }),
-//           {
-//             status: 404,
-//             headers: {
-//               "Content-Type": "application/json",
-//             },
-//           }
-//         );
-//       }
-//     } else {
-//       return new Response(
-//         JSON.stringify({
-//           error: "Invalid query parameter",
-//         }),
-//         {
-//           status: 400,
-//           headers: {
-//             "Content-Type": "application/json",
-//           },
-//         }
-//       );
-//     }
-//   } catch (error) {
-//     console.error("Error processing request:", error);
-
-//     return new Response(
-//       JSON.stringify({ error: "Internal server error: " + error }),
-//       {
-//         status: 500,
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//       }
-//     );
-//   }
-// };
 
 async function getPrograms(
   goal: string
