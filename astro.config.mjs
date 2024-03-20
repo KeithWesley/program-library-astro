8c0cb1debd4ec1e88b24c8ed75408d871e99efd8
KeithWesley <keithfayant@gmail.com>
2024-03-20 09:31:58 -0500

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 
 import netlify from "@astrojs/netlify";
+import node from "@astrojs/node";
 import alpinejs from "@astrojs/alpinejs";
 import htmx from "astro-htmx";
 import db from "@astrojs/db";
